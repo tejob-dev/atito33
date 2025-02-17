39999fbeaba69d0652443989fbaf571f083fcb99
SicadevdPc <tchimouj66@gmail.com>
2025-02-17 16:36:08 +0000

    Modif email temp

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -8,7 +8,7 @@
 <body style="margin:0;padding:0;background-color:#ffffff;">
     <div style="width:100%;margin:0 auto;font-family:Sans-Serif">
         <div style="height:40px;background-color:#3270FC;color:white;padding:8px 16px">
-            <h1 style="margin:0;color:#ffffff;font-size:2em">ATITO.NET</h1>
+            <h1 style="margin:0;color:#ffffff;font-size:2em">ATITO</h1>
         </div>
 
         <div style="background-color:#fafafa;margin:0;padding:24px">
