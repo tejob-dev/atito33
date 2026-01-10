f284677fe02b5fec7e442c5270a33f92d517ee61
TCHIMOU JUNIOR <tchimouj77@gmail.com>
2026-01-10 15:28:16 +0000

    up push error 2

--- resources/views/useradmin/dashboard-annoncelist.blade.php
+++ resources/views/useradmin/dashboard-annoncelist.blade.php
@@ -487,8 +487,6 @@
         <script src="/js/scripts.js"></script>
         <script src="/js/dashboard.js"></script>
 
-        <script defer async type="text/javascript" charset="utf-8" src="/js/charts.js"></script>
-
         <script src="/jsc/bootstrap.bundle.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
         <script src="/jsc/jquery.dataTables.min.js"></script>
@@ -679,5 +677,9 @@
 
             });
         </script>
+
+
+        
+     <script src="/js/charts.js"></script>
     </body>
 </html>
